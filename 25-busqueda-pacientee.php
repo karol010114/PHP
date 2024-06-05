c73529cff08c1d9a0963306edd2edfaf5bd3fe91
Nuevo Nombre <luis.laqui@gmail.com>
2024-06-05 18:02:15 -0500

    nuevo archivo

--- 25-busqueda-pacientee.php
+++ 25-busqueda-pacientee.php
@@ -4,14 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Busqueda de Pacientes</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body>
+<body>    
     <center><h1>Busqueda de Pacientes</h1></center>
     <!-- <form action="22.1-consulta-pdo.php" method="POST"> -->
     <input type="text" placeholder="Escribe aqui" name="nombre" id="nombre" /><br>
-    <label type="tos" name="tos">Tos</label><input type="checkbox" for="tos">
+    <label id="tos" name="tos">Tos</label><input type="checkbox" for="tos">
     <button type="button" onclick="buscarPacientes();">Buscar</button>
-    <table style="border: 1px solid black;">
+    <table style="border: 1px solid black;" id="tabla">
         <tr>
             <td>Paciente</td>
             <td>Edad</td>
@@ -20,10 +21,34 @@
             <td>Tos</td>
             <td>Fiebre</td>
             <td>Disnea</td>
-            <td>Acciones</td>         
-        </tr>
+            <td>Acciones</td>
+        </tr>          
     </table>
-    <!-- </gorm> -->
-    <script src="25.1-ajax.js"> </script>
+    <!-- </form> -->
+    <script type="text/javascript"
+    src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="25.1-ajax.js"></script>
+ 
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Nuevo Paciente
+    </button>
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Formulario</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                    aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <label for="name" class="form-label">Nombre:</label>
+                    <input type="text" class="form-control" id="name" name="name"/>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
+ 
